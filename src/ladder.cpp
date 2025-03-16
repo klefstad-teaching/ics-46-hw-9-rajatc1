a64771c42ee432eb73fe5858f8df2946c7b1fd91
rajatc1 <rajatc1@uci.edu>
2025-03-16 00:06:14 +0000

    c

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -72,17 +72,11 @@ void load_words(set<string>& word_list, const string& file_name) {
 }
 
 void print_word_ladder(const vector<string>& ladder) {
-    if (ladder.empty()) {
-        cout << "No word ladder found.\n";
-        return;
-    }
-
-    cout << "Word ladder found: ";
     for (size_t i = 0; i < ladder.size(); ++i) {
         cout << ladder[i];
-        if (i < ladder.size()) cout << " "; // Add space between words, but not after the last word
+        if (i < ladder.size()) cout << " -> ";
     }
-    cout << "\n"; // Add a newline at the end
+    cout << endl;
 }
 
 void verify_word_ladder() {
